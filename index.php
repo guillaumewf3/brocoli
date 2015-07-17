96dc82e608c49f8f123166dea3a2b3237f283346
Florian Wouane <Wouane@users.noreply.github.com>
2015-07-17 12:13:48 +0200

    Add image brocolis

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 </head>
 <body>
 dsafdasfdsfadsf
+	<img src="http://mktg.factosoft.com/consoglobe/image-upload/img/broccoli.jpg" alt="brocolis">
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima vitae iste, ad voluptate deleniti repudiandae amet officia laudantium est, beatae voluptates natus officiis quidem ipsam nemo quas suscipit quisquam. Dicta.</p>
 	1111
 
