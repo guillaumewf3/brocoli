5e60b159fb3eb7cf55ad8f22559d1753aceb8dac
guillaumewf3 <guillaumewf3@gmail.com>
2015-07-17 12:27:04 +0200

    remove part of p

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <body>
 dsafdasfdsfadsf
 	<img src="http://mktg.factosoft.com/consoglobe/image-upload/img/broccoli.jpg" alt="brocolis">
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima vitae iste, ad voluptate deleniti repudiandae amet officia laudantium est, beatae voluptates natus officiis quidem ipsam nemo quas suscipit quisquam. Dicta.</p>
+	<p>Lorem ipsum dolor sit ametpit quisquam. Dicta.</p>
 	1111
 
 	<script src="js/broco.js"></script>
