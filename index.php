4f9cb9d5169c49eae497a9026af79e0509b3d586
Florian Wouane <Wouane@users.noreply.github.com>
2015-07-17 12:24:57 +0200

    delete first p, add new p 'coucou'

--- index.php
+++ index.php
@@ -8,9 +8,8 @@
 <body>
 dsafdasfdsfadsf
 	<img src="http://mktg.factosoft.com/consoglobe/image-upload/img/broccoli.jpg" alt="brocolis">
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima vitae iste, ad voluptate deleniti repudiandae amet officia laudantium est, beatae voluptates natus officiis quidem ipsam nemo quas suscipit quisquam. Dicta.</p>
 	1111
-
+	<p>Coucou tout le monde :D</p>
 	<script src="js/broco.js"></script>
 </body>
 </html>
