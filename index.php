5338c4b631f2b65462840b7f56e0633019d6ddf9
guillaumewf3 <guillaumewf3@gmail.com>
2015-07-17 11:08:15 +0200

    add main stylesheet

--- index.php
+++ index.php
@@ -3,8 +3,11 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Brocoli</title>
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima vitae iste, ad voluptate deleniti repudiandae amet officia laudantium est, beatae voluptates natus officiis quidem ipsam nemo quas suscipit quisquam. Dicta.</p>
+	
 </body>
 </html>
